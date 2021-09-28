432ff0684eed7a8e16daac6369f330410d9f9e31
roberque <danielroberque@hotmail.com>
2021-09-28 11:50:24 -0300

    Update man.py

--- man.py
+++ man.py
@@ -1,15 +1,4 @@
-# Our main file.
-
 import speech_recognition as sr
+import pyttsx3
 
-# Cria um reconhecedor
-r = sr.Recognizer()
-
-# Abrir o microfone para captura
-with sr.Microphone() as source:
-    r.adjust_for_ambient_noise(s)
-    while True:
-  audio = r.listen(source) # Define microfone python como fonte de audio
-
-   speech = r.recognize_google(audio,language='pt')
-   print('Você disse:', speech)
+audio = sr.Recognizer()
